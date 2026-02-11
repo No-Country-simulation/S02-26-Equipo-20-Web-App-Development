0e73756af337ccf05173f32aac5ce8c789dae18a
JaviCGz <javier.caceresgimenez@gmail.com>
2026-02-11 15:09:20 -0300

    feat(auth): create AuthController to handle authentication endpoints

---
+++ backend/src/main/java/com/nocountry/backend/auth/AuthController.java
@@ -0,0 +1,23 @@
+package com.nocountry.backend.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth") //En caso de cambiarla, modificarla también en el controlador y avisar al equipo de frontend
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
