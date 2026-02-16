4f85e5a78ce42fad77a11d4ee534a67a166b140b
Eduardo Maravilla <eduardomaravillag@gmail.com>
2026-02-16 14:30:25 -0600

    Initial microservice video project structure

---
+++ microservice-video/src/models/__init__.py
@@ -0,0 +1,5 @@
+from .instructions_video import InstructionsVideo
+from .video_job import VideoJob
+from .video_result import VideoResult
+
+__all__ = ["InstructionsVideo", "VideoJob", "VideoResult"]
