0f804eea1a7bc851f05c81109d92ac2bcf57e16b
Eduardo Maravilla <eduardomaravillag@gmail.com>
2026-02-20 11:41:13 -0600

    change name Video to VideoIn and update atributes

--- backend/src/main/java/com/nocountry/backend/model/VideoIn.java
+++ backend/src/main/java/com/nocountry/backend/model/VideoIn.java
@@ -1,53 +1,50 @@
 package com.nocountry.backend.model;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "videos")
+@Table(name = "videos_in")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Video {
+@EntityListeners(AuditingEntityListener.class)
+public class VideoIn {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_video_in")
     private Long id;
 
-    @Column(nullable = false)
-    private String url;
-
-    @Column(name = "public_id")
-    private String publicId;
-
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-
-    @Column(nullable = false)
-    private boolean deleted = false;
-
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "id_user", nullable = false)
     private User user;
 
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-    }
+    @Column(name = "path", nullable = false)
+    private String path;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "duration", nullable = false)
+    private String duration;
+
+    @Column(name = "video_size", nullable = false)
+    private String videoSize;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted", nullable = false)
+    private boolean deleted = false;
 
 }
