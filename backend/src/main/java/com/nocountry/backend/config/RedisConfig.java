72bcc617063ae92a2873ad243ab6fbf91db69980
Eduardo Maravilla <eduardomaravillag@gmail.com>
2026-02-20 19:19:36 -0600

    add redis configurations

---
+++ backend/src/main/java/com/nocountry/backend/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.nocountry.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import tools.jackson.databind.ObjectMapper;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    @Bean
+    public RedisSerializer<Object> redisSerializer(ObjectMapper objectMapper) {
+        return new GenericJacksonJsonRedisSerializer(objectMapper);
+    }
+
+}
