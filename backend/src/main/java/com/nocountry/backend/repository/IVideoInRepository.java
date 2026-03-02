2fbbcde30905ebfe42b65eb3f2e21a6f394efa82
Eduardo Maravilla <eduardomaravillag@gmail.com>
2026-03-02 11:53:43 -0600

    add funcionality for managing video original

--- backend/src/main/java/com/nocountry/backend/repository/IVideoInRepository.java
+++ backend/src/main/java/com/nocountry/backend/repository/IVideoInRepository.java
@@ -3,6 +3,7 @@ package com.nocountry.backend.repository;
 import java.util.List;
 import java.util.Optional;
 
+import com.nocountry.backend.model.VideoState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nocountry.backend.model.VideoIn;
@@ -21,4 +22,9 @@ public interface IVideoInRepository extends JpaRepository<VideoIn, Long> {
       AND vo.deleted = false
 """)
     List<Object[]> findVideoInIdAndVideoOutIdByUser(Long userId);
+
+    @Query("UPDATE VideoIn vi SET vi.deleted = true WHERE vi.path = :path")
+    void updateVideoInDeletedToTrue(String path);
+
+    boolean existsByPathAndDeletedFalseAndVideoState(String path, VideoState videoState);
 }
